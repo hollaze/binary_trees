a9c9c1256e20271086d348c6467f80c085e1db57
hollaze <deperiers.a@gmail.com>
2021-07-21 11:06:06 +0200

    insert node right

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,16 +1,22 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_insert_right - insert node right
+ * @parent: parent node
+ * @value: data : int
+ * Return: new node
+ */
+
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
 	new_node = binary_tree_node(parent, value);
 
+	new_node->right = parent->right;
+
 	if (parent->right)
-	{
-		new_node->right = parent->right;
 		new_node->right->parent = new_node;
-	}
 
 	parent->right = new_node;
 
