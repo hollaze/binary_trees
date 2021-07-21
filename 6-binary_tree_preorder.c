957a000735ed46a46431f382a11ebe93743698f4
hollaze <deperiers.a@gmail.com>
2021-07-21 11:17:32 +0200

    traversal: preorder

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,5 +1,11 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_preorder - tree traversal: preorder - Root, Left, Right
+ * @tree: actual node
+ * @func: function to call for each node
+ */
+
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
