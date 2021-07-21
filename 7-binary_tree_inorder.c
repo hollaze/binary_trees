c6646f7bea695a959077640b5dad028bd49d1719
hollaze <deperiers.a@gmail.com>
2021-07-21 11:18:28 +0200

    traversal: inorder

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,5 +1,11 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_inorder - tree traversal: preorder -  Left, Root, Right
+ * @tree: actual node
+ * @func: function to call for each node
+ */
+
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
