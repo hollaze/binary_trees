1ae846dd2e0e3e74653018e8b50d16ea4f6acd15
hollaze <deperiers.a@gmail.com>
2021-07-21 11:20:14 +0200

    traversal: postorder

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,5 +1,11 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_postorder - tree traversal: postorder - Left, Right, Root
+ * @tree: actual node
+ * @func: function to call for each node
+ */
+
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
@@ -7,5 +13,5 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 
 	binary_tree_postorder(tree->left, func);
 	binary_tree_postorder(tree->right, func);
-    func(tree->n);
+	func(tree->n);
 }
