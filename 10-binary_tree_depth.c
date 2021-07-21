7cd5af660486d74d7be9e6f2ecb6601037538714
hollaze <deperiers.a@gmail.com>
2021-07-21 11:37:55 +0200

    Count binary tree depth

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,5 +1,11 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_depth - count binary tree depth
+ * @tree: actual node
+ * Return: depth count
+ */
+
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	if (tree == NULL || tree->parent == NULL)
